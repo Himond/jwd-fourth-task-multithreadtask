e2c8883324f5c529abbe6ce4579f577b879cdf0f
Ihar Litvinko <i.litvinko.minsk@mail.ru>
2021-07-29 10:57:01 +0300

    Final commit v1

--- src/main/java/by/training/multithreadtunnel/main/Main.java
+++ src/main/java/by/training/multithreadtunnel/main/Main.java
@@ -7,7 +7,9 @@ import by.training.multithreadtunnel.parser.TrainParser;
 import by.training.multithreadtunnel.reader.ReaderService;
 import by.training.multithreadtunnel.reader.impl.ReaderServiceImpl;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -15,8 +17,13 @@ import java.util.concurrent.Executors;
 public class Main {
     public static void main(String[] args) throws TunnelException, IOException {
 
+        ClassLoader classLoader = Main.class.getClassLoader();
+        URL resource = classLoader.getResource("trains.txt");
+        assert resource != null;
+        String absolutePath = new File(resource.getFile()).getAbsolutePath();
         ReaderService service = new ReaderServiceImpl();
-        List<Train> trains = TrainParser.parseTrain(service.read("src\\main\\resources\\trains.txt"));
+
+        List<Train> trains = TrainParser.parseTrain(service.read(absolutePath));
         ExecutorService executorService = Executors.newFixedThreadPool(trains.size());
         trains.forEach(executorService::execute);
         executorService.shutdown();
